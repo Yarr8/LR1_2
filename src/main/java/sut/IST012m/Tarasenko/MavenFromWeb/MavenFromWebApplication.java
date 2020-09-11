122e9254f8c02452921bb81f0585b001779c78f6
Yarr <ars3577@gmail.com>
2020-09-11 12:43:36 +0300

    Initial commit

---
+++ src/main/java/sut/IST012m/Tarasenko/MavenFromWeb/MavenFromWebApplication.java
@@ -0,0 +1,13 @@
+package sut.IST012m.Tarasenko.MavenFromWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenFromWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenFromWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sut/IST012m/Tarasenko/MavenFromWeb/MavenFromWebApplicationTests.java
@@ -0,0 +1,13 @@
+package sut.IST012m.Tarasenko.MavenFromWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenFromWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
